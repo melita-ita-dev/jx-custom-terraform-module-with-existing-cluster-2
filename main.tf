82d970a03d8875c59125c678f368c45c2062f553
Andrea Gaffiero <Ags19917melita>
2021-09-28 11:52:47 +0200

    Chore: commenting out unneeded variables

--- main.tf
+++ main.tf
@@ -7,23 +7,6 @@ terraform {
   required_version = ">= 0.12.0, < 0.15"
 }
 
-resource "google_container_cluster" "jx_cluster" {
-  #provider                = google-beta
-  #name                    = var.cluster_name
-  #description             = "jenkins-x cluster"
-  #location                = var.cluster_location
-  #network                 = var.cluster_network
-  #subnetwork              = var.cluster_subnetwork
-  #enable_kubernetes_alpha = var.enable_kubernetes_alpha
-  #enable_legacy_abac      = var.enable_legacy_abac
-  #enable_shielded_nodes   = var.enable_shielded_nodes
-  #initial_node_count      = var.min_node_count
-  #logging_service         = var.logging_service
-  #monitoring_service      = var.monitoring_service
-}
-
-
-
 // ----------------------------------------------------------------------------
 // Configure providers
 // ----------------------------------------------------------------------------
