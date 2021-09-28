e9ad345fde26ce57ef2576e95f04c2b3b4f9b322
Andrea Gaffiero <Ags19917melita>
2021-09-28 10:21:56 +0200

    Chore: changes for resource + module

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -20,14 +20,14 @@ resource "google_container_cluster" "jx_cluster" {
   name                    = var.cluster_name
   description             = "jenkins-x cluster"
   location                = var.cluster_location
-  network                 = var.cluster_network
-  subnetwork              = var.cluster_subnetwork
-  enable_kubernetes_alpha = var.enable_kubernetes_alpha
-  enable_legacy_abac      = var.enable_legacy_abac
-  enable_shielded_nodes   = var.enable_shielded_nodes
-  initial_node_count      = var.min_node_count
-  logging_service         = var.logging_service
-  monitoring_service      = var.monitoring_service
+  #network                 = var.cluster_network
+  #subnetwork              = var.cluster_subnetwork
+  #enable_kubernetes_alpha = var.enable_kubernetes_alpha
+  #enable_legacy_abac      = var.enable_legacy_abac
+  #enable_shielded_nodes   = var.enable_shielded_nodes
+  #initial_node_count      = var.min_node_count
+  #logging_service         = var.logging_service
+  #monitoring_service      = var.monitoring_service
 
 
   //----added by david-----
