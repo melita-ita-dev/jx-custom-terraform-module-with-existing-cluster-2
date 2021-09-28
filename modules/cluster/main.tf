7dafcb1eef66f80af73f8d646bc4d782d55d0227
Andrea Gaffiero <Ags19917melita>
2021-09-28 11:50:49 +0200

    Chore: commenting out unneeded variables

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -6,8 +6,8 @@
 
 
 resource "google_container_cluster" "jx_cluster" {
-  provider                = google-beta
-  name                    = var.cluster_name
+  #provider                = google-beta
+  #name                    = var.cluster_name
   #description             = "jenkins-x cluster"
   #location                = var.cluster_location
   #network                 = var.cluster_network
