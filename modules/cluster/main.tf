9f88a28259cf6371d03fc2247eae2cd050a58969
Andrea Gaffiero <Ags19917melita>
2021-09-29 14:34:28 +0200

    Chore: added google_container_node_pool into cluster/main.tf

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -104,6 +104,50 @@ module "jx-health" {
 }
 
 // ----------------------------------------------------------------------------
+#Added by Andrea Gaffiero to be able to terraform import google_container_node_pool of existing cluster.
+
+resource "google_container_node_pool" "large_nodes" {
+  provider           = google-beta
+  name               = "large-nodes"
+  location           = "<my-location>"
+  cluster            = module.jx.cluster_name
+  initial_node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n2-standard-2"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.full_control",
+      "https://www.googleapis.com/auth/service.management",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    workload_metadata_config {
+      node_metadata = "GKE_METADATA_SERVER"
+    }
+
+    labels = {
+      preemptible = "true"
+    }
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
+
+  management {
+    auto_repair  = "true"
+    auto_upgrade = "false"
+  }
+}
+
+// ----------------------------------------------------------------------------
 // Add main Jenkins X Kubernetes namespace
 //
 // https://www.terraform.io/docs/providers/kubernetes/r/namespace.html
