a6f461923638c03562290f415bc39c45ec75f7bd
Andrea Gaffiero <Ags19917melita>
2021-10-01 09:11:23 +0200

    Chore: changed cluster access scope of logging from logging.admin to logging.write

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -10,11 +10,9 @@ locals {
     "https://www.googleapis.com/auth/devstorage.full_control",
     "https://www.googleapis.com/auth/service.management",
     "https://www.googleapis.com/auth/servicecontrol",
-    "https://www.googleapis.com/auth/logging.admin",
+    "https://www.googleapis.com/auth/logging.write",
     "https://www.googleapis.com/auth/monitoring",
 
-    #Changed https://www.googleapis.com/auth/logging.write to https://www.googleapis.com/auth/logging.admin
-    #Will change this after testing successful env deployment into existing cluster
   ]
 }
 resource "google_container_cluster" "jx_cluster" {
