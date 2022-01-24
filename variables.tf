1239ddf165ac1da7bd65dda55f845ebde0a43031
Andrea Gaffiero <Ags19917melita>
2022-01-24 13:10:28 +0100

    chore: adjust default due to terraform error of definition being too long

--- variables.tf
+++ variables.tf
@@ -48,7 +48,7 @@ variable "cluster_location" {
 variable "node_locations" {
   description = "The location (region or zone) in which the cluster master will be created. If you specify a zone (such as us-central1-a), the cluster will be a zonal cluster with a single cluster master. If you specify a region (such as us-west1), the cluster will be a regional cluster with multiple masters spread across zones in the region"
   type        = list(string)
-  default     = ["europe-west3-c"]
+  default     = "europe-west3-c"
 }
 
 variable "cluster_network" {
